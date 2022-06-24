6ea6f35a0c394a42dbbb401b8a770dffdf5dd37e
Bruno Gusmão <brunogusmao43@gmail.com>
2022-06-24 14:49:51 -0300

    .

--- src/api.js
+++ src/api.js
@@ -3,7 +3,44 @@ const API_KEY = "9efda158e5b1ee1a28ae30291e8ad89b";
 const categories = [
     {
         name: "trending",
-        titile: "Em alta",
-        path: ""
+        title: "Em alta",
+        path: `/trending/all/week?api_key=${API_KEY}&language=pt-br`,
+    },
+    {
+        name: "animeflixOriginals",
+        title: "Originais Animeflix",
+        path: `/discover/tv?api_key=${API_KEY}&language=pt-br`,
+    },
+    {
+        name: "topRated",
+        title: "Populares",
+        path: `/movie/top_rated?api_key=${API_KEY}&language=pt-br`,
+    },
+    {
+        name: "comedy",
+        title: "Comédias",
+        path: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
+    },
+    {
+        name: "romances",
+        title: "Romances",
+        path: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+    },
+    {
+        name:"documentarios",
+        title: "Documentários",
+        path: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+    },
+];
+
+
+export const getMovies = async (path) => {
+    try {
+        let url = `https://api.themoviedb.org/3/${path}`;
+        const response = await fetch(url);
+        return await response.json();
+
+    } catch (error) {
+        console.log("error erroe getMovies: ", error);
     }
-]
+};
